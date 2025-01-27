cd994edb945657e0f9bbf51c9bbdf2f5e71c6ea7
Santiago Aristizabal <aristizabalsantiago482@gmail.com>
2025-01-26 21:43:15 -0500

    fix: implement create functionality in view controller to associate product and user

--- src/api/view/controllers/view.ts
+++ src/api/view/controllers/view.ts
@@ -2,6 +2,24 @@
  * view controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::view.view');
+export default factories.createCoreController(
+  "api::view.view",
+  ({ strapi }) => ({
+    async create(ctx) {
+      const { data } = ctx.request.body;
+
+      const product = await strapi.documents("api::product.product").findOne({
+        documentId: data.product,
+      });
+      const response = await strapi.documents("api::view.view").create({
+        data: {
+          product: product.id,
+          user: data.user,
+        },
+      });
+      ctx.body = response;
+    },
+  })
+);
